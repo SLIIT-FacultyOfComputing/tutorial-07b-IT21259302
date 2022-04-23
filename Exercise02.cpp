dac9b0d94c4843344969b3a2de6695fd8b19c362
/IT21259302 </nirmithnew@gmail.com>
2022-04-23 19:15:44 +0530

    Done

--- Exercise02.cpp
+++ Exercise02.cpp
@@ -15,14 +15,21 @@ int main() {
    // ======== DO NOT CHANGE THE CODE GIVEN ABOVE ====================
    
    // 1. Create a dynamic Rectangle type variable (pointer)
+   Rectangle *rec1;
    // 2. Create a dynamic Rectangle Object set the length and width that was input from the keyboard
+   rec1 = new Rectangle(length, width);
    // 3. Create a dynamic Circle type variable (pointer)
+   Circle *circ1;
    // 4. Create a dynamic Circle Object set radius that was input from the keyboard
+   circ1 = new Circle(radius);
    // 5. Call the display method of the Rectangle Object
+   rec1->display();
    // 6. Call the display method of the Circle Object
+   circ1->display(); 
    // 7. Delete the Rectangle Object from memory
+   delete rec1;
    // 8. Delete the Circle Object from memory
-  
+  delete circ1;
   
    // ============ DO NOT CHANGE THE CODE BELOW ===================
    cout << "End of Program" << endl;
--- Item.cpp
+++ Item.cpp
@@ -3,8 +3,19 @@
 using namespace std;
 
 // 4. Implement Default Constructor Implementation
+Item :: Item(){
+  itemCode = 0;
+  unitPrice = 0.0;
+}
 // 5. Implement Overloaded Constructor Implementation
+Item :: Item(int itCode, float unPrice) {
+  itemCode = itCode;
+  unitPrice = unPrice;
+}
 // 6. Implement Destructor (display "Destructor Called")
+Item :: ~Item() {
+  cout << "Destructor called" << endl;
+}
 
 void Item::setDiscount(float pdiscount) {
   discount = pdiscount;
